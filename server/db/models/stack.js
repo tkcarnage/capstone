c80baf487b79c4c55b01a94cc6e3aa56e24f8eb7
Nate Chastain <nchastain@gmail.com>
2016-05-08 21:39:36 -0400

    Revised job/stack model - I want to take care of the CRON job specifications from within the model, also move result stuff to individual tests, then we can just loop through to see if all the tests in the stack passed

--- server/db/models/stack.js
+++ server/db/models/stack.js
@@ -2,11 +2,41 @@ const mongoose = require('mongoose');
 
 var stackSchema = new mongoose.Schema({
 	tests: [{type: mongoose.Schema.Types.ObjectId, ref: 'Test'}],
-	// results: [{type: mongoose.Schema.Types.ObjectId, ref: 'Result'}], // result should be a property on each test, not the stack
 	lastRun: {  
 		type: Date,
 		default: null
-	} 
+	},
+	// use this model prop to direct the CRON job
+	// the properties in frequency correspond to CRON timing properties
+	// but on the front-end, we simply show "hourly" or "every day at 10:52 P.M."
+	autorun: { // 
+		status: {
+			type: Boolean,
+		},
+		frequency: {
+			minute: {
+				type: String,
+				default: '*',
+			},
+			hour: {
+				type: String,
+				default: '*',
+			},
+			dayofmonth: {
+				type: String,
+				default: '*',
+			},
+			month: {
+				type: String,
+				default: '*',
+			},
+			dayofweek: {
+				type: String,
+				default: '*',
+			},
+		},
+		
+	}
 });
 
 mongoose.model('Stack', stackSchema);
