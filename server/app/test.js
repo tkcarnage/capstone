7a8210002456b8bf97cd7f011b001041e8d70d60
David Zausner <zausnerd@gmail.com>
2016-05-12 15:58:09 -0400

    no debugging done yet

--- server/app/test.js
+++ server/app/test.js
@@ -17,45 +17,60 @@ function isDiff(obj1, obj2) {
 	return true;
 }
 
-function buildPOST() {
 
-}
-
-function buildDelete() {
-
-}
-
-function builPUT() {
-
-}
-
-function buildTest(test) {
-  var curlString = '';
-	if (test.method == 'GET') { curlString = buildGET(test); }
-  else if (test.method == 'POST') { curlString = buildPOST(test); }
-  else if (test.method == 'PUT') { curlString = buildPUT(test); }
-  else { curlString = buildDelete(test); }
-  var response =  sendRequest(curlString);
-  return response;
-}
-
-function buildGET(test) {
-  var string = '';
-  if (test.headers) {
-    for (var i = 0; i < test.headers.length - 1; i++) {
-      if (test.headers[i] !== undefined && test.headers[i] !== null && test.headers[i] !== '{}') {
-        var header = '-H'+ '%' + test.headers[i].key + ':' + test.headers[i].value +  '%';
-        string += header;
+var curlBuilder = {
+  headers: function(test, curlString) {
+    test.headers.forEach(function(elem) {
+      if (elem.key != undefined) {
+        var string = '-H'+ '%' + elem.key + ':' + elem.value;
+        curlString += string;
       }
+    });
+    return curlString;
+  },
+  url: function(test,curlString) {
+    curlString += '%' + test.url;
+    return curlString;
+  },
+  authorization: function(test, curlString) {
+    var authString = '%--user' + '%' + test.authorization.email + ':' + test.authorization.password;
+    curlString += authString;
+    return curlString;
+  },
+  body: function(test, curlString) {
+    var string = '';
+    if (test.body.bodytype == 'x-www-form-urlencoded') {
+      string += '-d%' + test.body.data;
+    } else if (test.body.bodytype === 'raw') {
+      string += '-d%' + test.body.data;
+      string += '%-H'+ '%' + 'Content-type' + ':' + 'application/json';
+    } else if (test.body.bodytype == 'form-data') {
+        var arrayObj = JSON.parse(test.body.data);
+        string += '%-F';
+        arrayObj.forEach(function(elem) {
+          if (elem.key!= undefined) { string += elem.key + "=" + elem.value + '&'; }
+        });
+        string = string.substring(0,string.length - 1);
+    }
+    curlString += string;
+    return curlString;
+  }
+}
+
+function buildCurlString(testObj) {
+  var curlString = '-X%' + testObj.method + '%';
+  for (var key in curlBuilder) {
+    if (testObj[key]) {
+      curlString = curlBuilder[key](testObj, curlString);
     }
   }
-  string += test.url;
-  return string;
+  return sendRequest(curlString);
 }
 
 
 function sendRequest(curlString) {
 	var array = curlString.split('%');
+  console.log(array);
   const response = spawn('curl', array);
   if (response.stdout == null) {
   	return response.stderr.toString();
@@ -63,12 +78,18 @@ function sendRequest(curlString) {
   return response.stdout.toString();
 }
 
+
 //testObj = {url:"https://httpbin.org/get?show_env=1", headers: [{key: "testing",value: "123"}, {key: "hi",value: "friend" }, {key: "myNameIs",value: "slimShady" }], method: 'GET' };
 //testObj = {url:"https://httpbin.org/get?show_env=1", method: 'GET' };
-var testObj =  {"user":{"_id":"57320be92c0bed2837117814","email":"user@user.com","__v":0,"isAdmin":false,"jobs":[]},"url":"http://httpbin.org/get","params":[],"headers":[{"key":"hi","value":"friend"},{}],"body":{"data":[]},"method":"GET","name":"david"}
-//var testObj =  {"user":{"_id":"57320be92c0bed2837117814","email":"user@user.com","__v":0,"isAdmin":false,"jobs":[]},"url":"http://httpbin.org/get","params":[],"body":{"data":[]},"method":"GET","name":"david"}
-console.log(buildTest(testObj));
+var testObj =  {"user":{"_id":"57320be92c0bed2837117814","email":"user@user.com","__v":0,"isAdmin":false,"jobs":[]},"url":"http://httpbin.org/get", "authorization" : {email: "zausnerd@gmail.com", password: "password"}, "params":[],"headers":[{"key":"hi","value":"friend"},{}],"body":{"data":[]},"method":"GET","name":"david"}
+//var testObj =  {"user":{"_id":"57320be92c0bed2837117814","email":"user@user.com","__v":0,"isAdmin":false,"jobs":[]},"url":"https://api.github.com/users/zausnerd", "authorization" : {email: "zausnerd", password: "password1"}, "params":[],"headers":[{"key":"hi","value":"friend"},{}],"body":{"data":[]},"method":"GET","name":"david"}
 
+
+//var testObj =  {"user":{"_id":"57320be92c0bed2837117814","email":"user@user.com","__v":0,"isAdmin":false,"jobs":[]},"url":"http://httpbin.org/get","params":[],"body":{"data":[]},"method":"GET","name":"david"}
+
+
+
+console.log(buildCurlString(testObj));
 
 
 
