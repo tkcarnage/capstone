179fd7347f0cf8c4e105430d50b3f9c36858b3b1
Nate Chastain <nchastain@gmail.com>
2016-05-16 18:11:21 -0400

    Added ngDraggable

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -11,11 +11,14 @@ module.exports = function (app) {
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
     var assetPath = path.join(root, './assets');
+    var bowerPath = path.join(root, './bower_components');
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
     app.use(express.static(assetPath));
+    app.use(express.static(bowerPath));
+
 
 };
