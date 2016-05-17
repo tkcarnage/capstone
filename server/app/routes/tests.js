22bac99fd3c9eade5cd198a946c98347ee9eb981
Victor Hom <vh62@cornell.edu>
2016-05-17 12:00:20 -0400

    restores code review files

---
+++ server/app/routes/tests.js
@@ -0,0 +1,54 @@
+'use strict';
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const Auth = require('../../utils/auth.middleware');
+const Test = mongoose.model('Test');
+
+module.exports = router;
+
+router.get('/:testid', Auth.assertAuthenticated, function(req,res,next) {
+    const testid = req.params.testid;
+    Test.findById(testid)
+    .then(function(test){
+        res.json(test);
+    })
+    .catch(next);
+});
+
+router.post('/', Auth.assertAuthenticated, function(req,res,next) {
+    Test.create(req.body)
+    .then(function(test){
+        res.json(test);
+    })
+    .catch(next);
+});
+
+router.put('/:testid', Auth.assertAuthenticated, function(req,res,next) {
+    const testid = req.params.testid;
+    Test.findById(testid)
+    .then(function(test){
+        test.set(req.body);
+        return test.save();
+    })
+    .then(function(test){
+        res.json(test);
+    })
+    .catch(next);
+});
+
+router.get('/', Auth.assertAuthenticated, function(req, res, next) {
+    let queryObj = {};
+    if (req.query.userId) queryObj = {user: req.query.userId};
+    Test.find(queryObj)
+    .then(tests =>{
+        console.log("hello");
+        res.json(tests);
+    })
+    .catch(next);
+});
+
+router.delete('/:testid', Auth.assertAuthenticated, function(req, res, next){
+   const testid = req.params.testid;
+   Test.findById(testid).remove().exec()
+   .catch(next);
+});
