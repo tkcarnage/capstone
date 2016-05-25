84d5783b1ab99746c5cac13f07c677543843708b
seanbert4 <seanbert4@gmail.com>
2016-05-25 09:42:17 -0400

    removed everything I added

--- server/app/configure/parsing-middleware.js
+++ server/app/configure/parsing-middleware.js
@@ -1,19 +1,19 @@
 'use strict';
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
 
 module.exports = function (app) {
 
